d87a84305c97245cd2546fd8d7080d6ede0fd60b
Jeremey Lee <leejeremey@gmail.com>
2019-08-11 17:05:22 -0700

    created POST request to generate shortened URL

--- controllers/shortener.js
+++ controllers/shortener.js
@@ -1,5 +1,6 @@
 const shortenerRouter = require('express').Router();
 const validUrl = require('valid-url');
+const nanoid = require('nanoid');
 const UrlShortener = require('../models/UrlShortener');
 
 shortenerRouter.get('/', async (req, res, next) => {
@@ -12,12 +13,29 @@ shortenerRouter.get('/', async (req, res, next) => {
 
 shortenerRouter.post('/', async (req, res, next) => {
   const { body } = req;
+  console.log(body);
   try {
     if (validUrl.isUri(body.url)) {
-      const foundUrl = UrlShortener.find({ originalUrl: body.url });
+      const foundUrl = await UrlShortener.findOne({ originalUrl: body.url });
+      console.log('foundUrl: ', foundUrl);
+      if (foundUrl) {
+        console.log('here');
+        res.send(`/${foundUrl.shortenUrl}`);
+      } else {
+        const shortenUrl = nanoid(10);
+        console.log(shortenUrl);
+        const newShortenUrl = new UrlShortener({
+          originalUrl: body.url,
+          shortenUrl,
+        });
+        await newShortenUrl.save();
+        res.status(200).send(`/${shortenUrl}`);
+      }
     } else {
       res.status(400).end();
     }
+  } catch (exception) {
+    next(exception);
   }
 
 });
