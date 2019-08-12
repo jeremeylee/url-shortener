47213417c474901426a8df5f07c072814892a26e
Jeremey Lee <leejeremey@gmail.com>
2019-08-11 18:08:37 -0700

    created form for url submission

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Row, Typography, Input, Button } from 'antd';
+
+const { Title } = Typography;
 
 const App = (props) => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+  }
   return (
     <div>
-      <h1>Hello WOrld</h1>
+      <Row type="flex" justify="center">
+        <Title>Url Shortener</Title>
+      </Row>
+      <Row type="flex" justify="center">
+        <form onSubmit={handleSubmit}>
+          <label>Enter a valid URL:</label>
+          <Input onChange={event => setInput(event.target.value)} placeholder="www.google.com" value={input}/>
+          <Button htmlType="submit">Shorten!</Button>
+        </form>
+      </Row>
     </div>
   );
 }
